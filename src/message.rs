dd8a0af8427f3de587029c9ad9bb48335b4e1309
Charlie FG <cfg@zunautica.org>
2015-06-09 17:17:02 +0100

    Add msg_put

--- src/message.rs
+++ src/message.rs
@@ -1,7 +1,7 @@
 extern crate libc;
 extern crate rsnl;
 
-use libc::{c_int};
+use libc::{c_int, c_void};
 use rsnl::message::{NetlinkMessage, nl_msg, nlmsghdr};
 use rsnl::message::expose::{nl_msg_ptr, nlmsghdr_ptr};
 
@@ -12,6 +12,7 @@ struct genlmsghdr;
 extern "C" {
     fn genlmsg_valid_hdr(hdr: *const nlmsghdr, hdrlen: c_int) -> i32;
     fn genlmsg_hdr(hdr: *const nlmsghdr) -> *const genlmsghdr;
+    fn genlmsg_put(msg: *const nl_msg, port: u32, seq: u32, family: c_int, hdrlen: c_int, flags: c_int, cmd: u8, version: u8) -> *const c_void;
 }
 
 // leaky!
@@ -29,4 +30,8 @@ pub fn hdr(msg: &NetlinkMessage) -> GenlHeader {
     GenlHeader {
         ptr: unsafe { genlmsg_hdr(nlmsghdr_ptr(msg)) }
     }
+}
+
+pub fn put(msg: &mut NetlinkMessage, port: u32, seq: u32, family: i32, hdrlen: i32, flags: i32, cmd: u8, version: u8) {
+    unsafe { genlmsg_put(nl_msg_ptr(msg), port, seq, family, hdrlen, flags, cmd, version); }
 }
